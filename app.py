0223c0d1720e8b2203145690bd7e0039e5260558
Sami Richards <1902765@uad.ac.uk>
2023-04-07 17:38:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-from flask import Flask, request
-
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/feedback', methods=['POST'])
-def handle_feedback():
-    location_id = request.form['location_id']
-    feedback = request.form['feedback']
-
-    #Initial stuff, just get it working for now, need to add API keys and stuff
-
-    return 'Success'
-
 @app.route('/test')
-def handle_test():
-    return 'Hello World!'
-
+def index():
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run()
